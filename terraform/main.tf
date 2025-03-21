cab1e960a921657bbed8064a07476992dae7a030
dhinasurya <dhinavarshu@gmail.com>
2025-03-21 12:16:33 +0530

    new storage account created

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "rare_rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_storage_account" "rare_storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rare_rg.name
+  location                 = azurerm_resource_group.rare_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
